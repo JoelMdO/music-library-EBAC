05bdb63d1626b43249f278c624b70c9401a40227
Joel Montes de Oca López <122069243+JoelMdO@users.noreply.github.com>
2026-01-09 11:39:31 +0300

    Revert "Revert "Module 3.1.0 - Style with Styled Components""

---
+++ src/styles/StyledBody.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const StyledMain = styled.main`
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  height: 100dvh;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: 100vw;
+    height: 80dvh;
+  }
+`;
+
+export const StyledAside = styled.aside`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 45vw;
+  height: 100dvh;
+  border-right: 2px solid var(--border-color);
+  position: relative;
+
+  @media (max-width: 768px) {
+    width: 100vw;
+    height: 20dvh;
+    margin-bottom: 1rem;
+  }
+`;
